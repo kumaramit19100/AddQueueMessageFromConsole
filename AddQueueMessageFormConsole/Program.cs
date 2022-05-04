94ac1fc362720b1704f1bfe2b50d63268e3e5734
amit kumar <kumaramit19100@gmail.com>
2022-05-04 15:18:35 +0530

    Initial Commit

---
+++ AddQueueMessageFormConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using CloudeStorageAccount = Microsoft.WindowsAzure.Storage.CloudStorageAccount;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+
+namespace AddQueueMessageFormConsole
+{
+    class Program
+    {
+        public static string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=akstaccount;AccountKey=gkGSx+sQsRaUtEU9YAl5cxogSchyKpV7qHl8pkJ+rhuA+yiwllI+0C249yF7cRTu6JrAGX5Dr3Uo8i7rGjQqbA==;EndpointSuffix=core.windows.net";
+        static void Main(string[] args)
+        {
+            AddMessage();
+            Console.ReadKey();
+        }
+
+        public static void AddMessage()
+        {
+            Console.WriteLine("Write Some message for add in Queue -");
+            string msg = Console.ReadLine();
+            CloudeStorageAccount cls = CloudeStorageAccount.Parse(ConnectionString);
+            CloudQueueClient queueClient = cls.CreateCloudQueueClient();
+            CloudQueue cloudQueue = queueClient.GetQueueReference("task-1");
+            CloudQueueMessage queueMessage = new CloudQueueMessage(msg);
+            if (queueMessage != null)
+            {
+                cloudQueue.AddMessage(queueMessage);
+                Console.WriteLine("Message Add Successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Please write some messgae for add in Queue !");
+                Console.Beep(500, 500);
+            }            
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+For use this program First add Some Nuget Package
+1 : WindowsAzure.Storage
+2 : Microsoft.Azure.WebJobs.Extensions.Storage
